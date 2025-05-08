0be34c7310a1c16458ea59260e5342eb654591db
Julien Degermann <julien@MacBookAir-001.lan>
2025-05-08 09:06:03 +0200

    add super user through .env

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,84 +6,55 @@ use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class UserFixtures extends Fixture
 {
 
   public function __construct(
-    private readonly UserPasswordHasherInterface $hasher
+    private readonly UserPasswordHasherInterface $hasher,
+    private readonly ParameterBagInterface $params
+
   ) {}
 
   public function load(ObjectManager $manager): void
   {
 
-    $users = [
-      'comments' => [
-        [
-          'userName' => "John",
-          'email' => "john@example.com"
-        ],
-        [
-          'userName' => "Mary",
-          'email' => "mary@example.com"
-        ],
-        [
-          'userName' => "Ash",
-          'email' => "ahs@example.com"
-        ]
-      ],
-      'any' => [
-        [
-          'firstName' => 'Jack',
-          'lastName' => 'Doe',
-        ],
-        [
-          'firstName' => 'Julien',
-          'lastName' => 'Degermann',
-        ],
-        [
-          'firstName' => 'Jean',
-          'lastName' => 'Marie',
-        ],
-      ]
-    ];
+    // $users = [
+    //   'comments' => [
+    //     [
+    //       'userName' => "John",
+    //       'email' => "john@example.com"
+    //     ],
+    //     [
+    //       'userName' => "Mary",
+    //       'email' => "mary@example.com"
+    //     ],
+    //     [
+    //       'userName' => "Ash",
+    //       'email' => "ahs@example.com"
+    //     ]
+    //   ],
+    //   'any' => [
+    //     [
+    //       'firstName' => 'Jack',
+    //       'lastName' => 'Doe',
+    //     ],
+    //     [
+    //       'firstName' => 'Julien',
+    //       'lastName' => 'Degermann',
+    //     ],
+    //     [
+    //       'firstName' => 'Jean',
+    //       'lastName' => 'Marie',
+    //     ],
+    //   ]
+    // ];
 
-
-    foreach ($users as $key => $userArray) {
-      foreach ($userArray as $userKey => $user) {
-
-        $current = new User();
-        if ($key === 'comments') {
-          $current->setUserName($user['userName']);
-          $current->setEmail($user['email']);
-        } else {
-          $current->setFirstName($user['firstName']);
-          $current->setLastName($user['lastName']);
-          $current->setEmail($user['firstName'] . '@example.com');
-        }
-        if ($key === 'comments') {
-          $this->addReference('commentUser' . $userKey, $current);
-        } else {
-          $this->addReference('user' . $userKey, $current);
-        }
-        $manager->persist($current);
-      }
-    }
-
-    // // add SuperAdmin
-    // $superAdmin = new User();
-    // $superAdmin
-    //   ->setUserName('SuperAdmin')
-    //   ->setEmail('admin@admin.com')
-    //   ->setRoles(['ROLE_SUPER_ADMIN', 'ROLE_ADMIN', 'ROLE_USER'])
-    //   ->setPassword($this->hasher->hashPassword($superAdmin, 'monMotDePasse123!'))
-    //   ->setFirstName('Super')
-    //   ->setLastName('Admin')
-    //   ->setBirthDate(new \DateTimeImmutable('1988-10-18'));
-    // $manager->persist($superAdmin);
 
     // foreach ($users as $key => $userArray) {
     //   foreach ($userArray as $userKey => $user) {
+
     //     $current = new User();
     //     if ($key === 'comments') {
     //       $current->setUserName($user['userName']);
@@ -102,13 +73,12 @@ class UserFixtures extends Fixture
     //   }
     // }
 
-    // add SuperAdmin
     $superAdmin = new User();
 
     $password = $this->hasher->hashPassword($superAdmin, 'monMotDePasse123!');
     $superAdmin
       ->setUserName('SuperAdmin')
-      ->setEmail('admin@admin.com')
+      ->setEmail($this->params->get('SUPER_ADMIN_EMAIL'))
       ->setRoles(['ROLE_SUPER_ADMIN', 'ROLE_ADMIN', 'ROLE_USER'])
       ->setPassword($password)
       ->setFirstName('Super')
