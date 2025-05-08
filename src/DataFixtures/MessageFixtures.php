aed3857162e2b5922b4ecb6f03cf56b86d506b03
Julien Degermann <julien@MacBookAir-001.lan>
2025-05-08 21:08:20 +0200

    update smtp settings

--- src/DataFixtures/MessageFixtures.php
+++ src/DataFixtures/MessageFixtures.php
@@ -13,18 +13,18 @@ class MessageFixtures extends Fixture implements DependentFixtureInterface
   public function load(ObjectManager $manager): void
   {
 
-    $messages = [];
-    for ($i = 0; $i < 10; $i++) {
-      $messages[] = "Message number $i";
-    }
+  //   $messages = [];
+  //   for ($i = 0; $i < 10; $i++) {
+  //     $messages[] = "Message number $i";
+  //   }
 
-    foreach ($messages as $key => $message) {
-      $current = new Message();
-      $current->setAuthor($this->getReference('user' . rand(0, 2), User::class));
-      $current->setText($message);
-      $manager->persist($current);
-    }
-    $manager->flush();
+  //   foreach ($messages as $key => $message) {
+  //     $current = new Message();
+  //     $current->setAuthor($this->getReference('user' . rand(0, 2), User::class));
+  //     $current->setText($message);
+  //     $manager->persist($current);
+  //   }
+  //   $manager->flush();
   }
 
   public function getDependencies(): array
